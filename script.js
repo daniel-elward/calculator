10bf4a37ebf6a6fd9b28b4a44dc89df1ff3b134a
LR18 <daniel.elward@hotmail.com>
2024-09-26 09:39:16 -0300

    first functions
    added the first functions for + - * /

---
+++ script.js
@@ -0,0 +1,29 @@
+function add (valueOne, valueTwo) {
+    let sum = valueOne + valueTwo
+
+    return sum
+}
+
+function subtract (valueOne, valueTwo) {
+    let sum = valueOne - valueTwo
+
+    return sum
+}
+
+function multiply (valueOne, valueTwo) {
+    let sum = valueOne * valueTwo
+
+    return sum
+}
+
+function divide (valueOne, valueTwo) {
+    let sum = valueOne / valueTwo
+
+    return sum
+}
+
+/* TESTING 
+
+let test = divide(2);
+console.log(test);
+*/
