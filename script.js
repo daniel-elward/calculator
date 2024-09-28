f4e97c78156ff7357ee73dc7ea5af9cecf9c9791
LR18 <daniel.elward@hotmail.com>
2024-09-28 12:27:14 -0300

    updating variables
    logic added to split the equation to three variables
    numOne, mathOperator, numTwo.

--- script.js
+++ script.js
@@ -1,3 +1,90 @@
+let dispValue;
+let secondDispValue;
+let dispValueArray= [];
+let numOne;
+let numTwo;
+let result;
+let mathOperator;
+
+// DOM selection
+let keypadDOM = document.querySelector('.keyPadNumbers');
+let operatorDOM = document.querySelector('.keyPadOperators');
+let displayDOM = document.querySelector('.display');
+let secondDisplayDOM = document.querySelector('.secondaryDisplay');
+let equalBtnDOM = document.getElementById('equal');
+let clearBtnDOM = document.getElementById('clearBtn')
+
+/*
+store key presses in dispValue, when operator is selected store it in mathOperator and take current state of dispValue, store that number in numOne, update secondDispValue and clear dispValue. user enters the next numbers and presses = take the new value of dispValue and store it in numTwo. += to secodDisplay and make the calculation
+
+//testing logs
+console.log(`dispValue = ${dispValue}`);
+console.log(`numOne = ${numOne}`);
+console.log(`numTwo = ${numTwo}`);
+console.log(`tempNum = ${tempNum}`);
+console.log(dispValueArray)
+
+*/
+
+//populate displayValue with users input
+keypadDOM.addEventListener('click', (event => {
+    
+    //updates with each selection but removes the initial 'undefined'
+    if(!dispValue) { dispValue = event.target.value;} 
+    else { dispValue += event.target.value;}
+    
+    console.log(`dispValue = ${dispValue}`);
+    displayDOM.innerHTML = `${dispValue}`;
+}));
+
+//math operator
+operatorDOM.addEventListener('click', (e) => {
+
+    //get the first set of numbers for the calculation and the math operator
+    if (dispValue != null) {
+        numOne = dispValue;
+        secondDispValue = dispValue;
+        dispValue = null;
+        secondDisplayDOM.innerHTML += secondDispValue;
+    }
+
+    mathOperator = e.target.value;
+    secondDisplayDOM.innerHTML += e.target.value;
+});
+
+//get the result of the calculation
+equalBtnDOM.addEventListener('click', () => {
+
+        numTwo = dispValue;
+        secondDispValue += mathOperator;
+        secondDispValue += dispValue;
+        dispValue = null;
+        secondDisplayDOM.innerHTML = secondDispValue;
+
+    console.log(`result = ${result}`);
+
+    console.log(`dispValue = ${dispValue}`);
+    console.log(`secondDispValue = ${secondDispValue}`);
+    console.log(`numOne = ${numOne}`);
+    console.log(`numTwo = ${numTwo}`);
+    console.log(`mathOperator = ${mathOperator}`);
+});
+
+
+
+
+
+
+
+
+//clear button
+clearBtnDOM.addEventListener('click', () => clear());
+
+function clear(){
+    dispValue = null;
+    displayDOM.innerHTML = "0000"
+}
+
 function add (valueOne, valueTwo) {
     let sum = valueOne + valueTwo
 
@@ -22,15 +109,6 @@ function divide (valueOne, valueTwo) {
     return sum
 }
 
-/* TESTING 
-let test = divide(2);
-console.log(test);
-*/
-
-let valueOne;
-let valueTwo;
-let operator;
-
 function operate (operator, valueOne, valueTwo) {
 
     if (operator === "+") { 
@@ -54,8 +132,3 @@ function operate (operator, valueOne, valueTwo) {
        return sum
     } 
 };
-
-/* TESTING
-let test = operate("/", 4, 2);
-console.log(test);
-*/
