665f17fc202a7e149dc33b0da458134f180588d6
Dan Elward <daniel.elward@hotmail.com>
2024-09-29 07:53:44 -0300

    finished calculator logic
    extended calculators logic. 
    
    only two sets of numbers will be calculated at any time.

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 let dispValue;
 let secondDispValue;
-let dispValueArray= [];
 let numOne;
 let numTwo;
 let result;
@@ -26,9 +25,20 @@ keypadDOM.addEventListener('click', (event => {
 
 //math operator
 operatorDOM.addEventListener('click', (event) => {
+    //if user enters a second operator selection, get the sum of the current selections then get the first set of numbers for the calculation and the math operator
+    if (numOne >= 0 && mathOperator != null) {
 
-    //get the first set of numbers for the calculation and the math operator
-    if (dispValue != null) {
+        numTwo = dispValue;
+        operate(mathOperator, numOne, numTwo);
+        mathOperator = null;
+        mathOperator = event.target.value;
+        secondDispValue = result;
+        secondDisplayDOM.innerHTML = secondDispValue;
+        dispValue = null;
+        displayDOM.innerHTML = dispValue;
+        numOne = result;
+        
+    } else if (dispValue != null) {
         numOne = dispValue;
         secondDispValue = dispValue;
         dispValue = null;
@@ -42,6 +52,11 @@ operatorDOM.addEventListener('click', (event) => {
 //get the result of the calculation
 equalBtnDOM.addEventListener('click', () => {
 
+    if (numOne === null || numTwo === null){
+        secondDisplayDOM.innerHTML = "Hey! ....";
+        displayDOM.innerHTML = "Maybe try entering some numbers?";
+    };
+
     numTwo = dispValue;
     secondDispValue += mathOperator;
     secondDispValue += dispValue;
@@ -52,91 +67,79 @@ equalBtnDOM.addEventListener('click', () => {
 
     //update numOne for the next calculation
     numOne = result;
+    
 
-    //console logs for testing
-    console.log(`result = ${result}`);
-    console.log(`dispValue = ${dispValue}`);
-    console.log(`secondDispValue = ${secondDispValue}`);
-    console.log(`numOne = ${numOne}`);
-    console.log(`numTwo = ${numTwo}`);
-    console.log(`mathOperator = ${mathOperator}`);
+
 });
 
 
 //clear button
-clearBtnDOM.addEventListener('click', () => clear());
+clearBtnDOM.addEventListener('click', clear);
 
 function clear(){
     dispValue = null;
     secondDispValue = null;
+    result = null;
+    numOne = null;
+    numTwo = null;
     secondDisplayDOM.innerHTML = "0000";
     displayDOM.innerHTML = "0000";
 }
 
 function add (numOne, numTwo) {
     result = Number(numOne) + Number(numTwo)
-
+    //result = result.toFixed(2);
     return result
 }
 
 function subtract (numOne, numTwo) {
     result = Number(numOne) - Number(numTwo)
-
+    //result = result.toFixed(2);
     return result
 }
 
 function multiply (numOne, numTwo) {
     result = Number(numOne) * Number(numTwo)
-
+    //result = result.toFixed(2);
     return result
 }
 
 function divide (numOne, numTwo) {
-
-    //if (numOne || numTwo <= 0) {
-
-    //    clear();
-    //    secondDispValue = "What is this....";
-    //    secondDisplayDOM.innerHTML = "What is this....";
-    //    dispValue = "No Bro, you can't do that!";
-    //    displayDOM.innerHTML = "No Bro, you can't do that!";
-    //} else {
-
     result = Number(numOne) / Number(numTwo)
+    //result = result.toFixed(2);
     return result
-    //}
 }
 
 function operate (mathOperator, numOne, numTwo) {
 
-    if (mathOperator === '+') {
+        if (mathOperator === '+') {
 
-        add(numOne, numTwo);
-        displayDOM.innerHTML = result;
-
-    } else if (mathOperator === '-') {
-
-        subtract(numOne, numTwo);
-        displayDOM.innerHTML = result;
-
-    } else if (mathOperator === '/') {
-        
-        if (numOne <= 0 || numTwo <= 0) {
-            clear();
-            secondDispValue = "What is this....";
-            secondDisplayDOM.innerHTML = "What is this....";
-            dispValue = "No Bro, you can't do that!";
-            displayDOM.innerHTML = "No Bro, you can't do that!";
-        } else {
-            divide(numOne, numTwo);
+            add(numOne, numTwo);
             displayDOM.innerHTML = result;
-            secondDisplayDOM.innerHTML = secondDispValue;
-        }
 
-    } else if (mathOperator === '*') {
+        } else if (mathOperator === '-') {
 
-        multiply(numOne, numTwo);
-        displayDOM.innerHTML = result;
+            subtract(numOne, numTwo);
+            displayDOM.innerHTML = result;
 
-    };
+        } else if (mathOperator === '*') {
+
+            multiply(numOne, numTwo);
+            displayDOM.innerHTML = result;
+
+        } else if (mathOperator === '/') {
+            
+            if (numOne <= 0 || numTwo <= 0) {
+                clear();
+                secondDispValue = "What is this....";
+                secondDisplayDOM.innerHTML = "What is this....";
+                dispValue = "No Bro, you can't do that!";
+                displayDOM.innerHTML = "No Bro, you can't do that!";
+            } else {
+                divide(numOne, numTwo);
+                displayDOM.innerHTML = result;
+                secondDisplayDOM.innerHTML = secondDispValue;
+            }
+
+        };
 };
